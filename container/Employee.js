778c0614f424bcda23a630c655ca791174173310
Uriel Rosario <urielrosario1010@gmail.com>
2021-03-10 22:01:33 -0500

    added container

--- container/Employee.js
+++ container/Employee.js
@@ -1,7 +1,21 @@
+// define and export employee class
 class Employee {
   constructor(name, email, id) {
     this.name = name;
     this.email = email;
     this.id = id;
   }
+  getName() {
+    return this.name;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getId() {
+    return this.id;
+  }
+  getRole() {
+    return "Employee";
+  }
 }
+module.exports = Employee;
